1c250c78b740a0f0ab80ea1ef3abdf9a3b5a642b
marcospsd <marcosp_sd@hotmail.com>
2022-05-05 09:56:13 -0300

    vai

--- fastcheckout/settings.py
+++ fastcheckout/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
         'USER': 'fastcheckout',
         'PASSWORD': 'Diniz@123',
         'PORT': '3306',
-        'HOST': '172.17.0.2',
-        #'HOST': '10.3.1.5'
+        #'HOST': '172.17.0.2',
+        'HOST': '10.3.1.5'
     }
 }
 
